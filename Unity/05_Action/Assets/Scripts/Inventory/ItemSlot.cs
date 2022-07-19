4af87a48b628c639ce35343eaee3fe1cfe94b15c
Kwonkuhwan <kkh3486@naver.com>
2022-07-19 17:15:45 +0900

    05.Action - Test_Inventory 씬 추가

--- Unity/05_Action/Assets/Scripts/Inventory/ItemSlot.cs
+++ Unity/05_Action/Assets/Scripts/Inventory/ItemSlot.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 변수 -----------------------------------------------------------------------------
+    // 슬롯에 있는 아이템(ItemData)
+    // 아이템 갯수(int)
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // 프로퍼티 --------------------------------------------------------------------------
+    // 슬롯에 있는 아이템(ItemData)
+    // 아이템 갯수(int)
+
+    // 함수 ------------------------------------------------------------------------------
+    // 슬롯에 아이템을 설정하는 기능
+    // 슬롯을 비우는 기능
+    // 아이템 갯수를 증가/감소시키는 함수
+    // 아이템을 사용하는 함수
+    // 아이템을 장비하는 함수
 }
